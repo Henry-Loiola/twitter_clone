d0e7f1e1d1e14a017031ca3ebeb3f6db62b4eaf1
Henry <henryloiola@gmail.com>
2025-02-24 19:40:52 -0300

    atualizando

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!c6a69ssmalu26kl#!))i4_o@+u*(%3z-n&f14cc=u7p=@l*=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'henry0202.pythonanywhere.com']
 
 
 # Application definition
